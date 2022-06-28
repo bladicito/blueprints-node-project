66a1aa7104d586298c9a5568dfac89ec15b99a45
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2022-06-28 11:59:12 +0200

    TVBACKEND-1720: adds frontend directory.

---
+++ src/plugins/api-replies/index.ts
@@ -0,0 +1,22 @@
+import { FastifyError, FastifyInstance, FastifyPluginCallback, FastifyPluginOptions } from 'fastify';
+import fastifyPlugin from 'fastify-plugin';
+import { EReplyTypes } from '../../utils/enums/ReplyTypes.enum';
+
+const pluginCallback: FastifyPluginCallback = (
+  fastify: FastifyInstance,
+  options: FastifyPluginOptions,
+  done: (error?: FastifyError) => void
+): void => {
+  fastify.decorateReply('uncached', function ({ type, content }: { type: EReplyTypes; content: any }) {
+    // arrow function breaks binding this to fastify instance
+    // @ts-ignore
+    return this.type(type)
+      .header('Cache-Control', 'no-cache, no-store, must-revalidate')
+      .header('Pragma', 'no-cache')
+      .header('Expires', '0')
+      .send(content);
+  });
+
+  done();
+};
+export default fastifyPlugin(pluginCallback);
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,23 @@
 import fastify, { FastifyInstance } from 'fastify';
 import configDefault from '../config';
 import { EReplyTypes } from '../utils/enums/ReplyTypes.enum';
+import serverUtils from '../plugins/api-replies';
 
 export const config = configDefault;
 const server: FastifyInstance = fastify({ logger: { level: config.logs.level } });
 export const logger = server.log;
 
+declare module 'fastify' {
+  interface FastifyReply {
+    uncached: (options: { type: EReplyTypes; content: any }) => void;
+  }
+}
+
+server.register(serverUtils);
+
 // Health route
 server.get('/ping', (request, reply) => {
-  reply.type(EReplyTypes.TEXT).send('pong');
+  reply.uncached({ type: EReplyTypes.TEXT, content: 'pong' });
 });
 
 export default server;
